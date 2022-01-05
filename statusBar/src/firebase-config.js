10f8f504247e7b624da2f74bee29aa640f21cd8d
Võ Tuệ Nam <57885184+VoTueNam@users.noreply.github.com>
2022-01-05 11:56:34 +0700

    Add files via upload

---
+++ statusBar/src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJgyWOPITjIg-s4wDjcubWq43de-PwUwE",
+    authDomain: "react-login-3block.firebaseapp.com",
+    projectId: "react-login-3block",
+    storageBucket: "react-login-3block.appspot.com",
+    messagingSenderId: "245069606140",
+    appId: "1:245069606140:web:5db28efa6869dcfb0a3b79",
+    measurementId: "G-EEGTK5P97P",
+};
+
+export const app = initializeApp(firebaseConfig);
